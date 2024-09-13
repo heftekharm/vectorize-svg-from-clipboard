01e9960e0569d5c5e38e45b9f556c68faabceeaf
GitHub Action <action@github.com>
2024-09-13 16:32:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/heftekharm/vectorizesvgfromclipboard/MyBundle.kt
+++ src/main/kotlin/com/github/heftekharm/vectorizesvgfromclipboard/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.heftekharm.vectorizesvgfromclipboard
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/heftekharm/vectorizesvgfromclipboard/services/MyProjectService.kt
+++ src/main/kotlin/com/github/heftekharm/vectorizesvgfromclipboard/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.heftekharm.vectorizesvgfromclipboard.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heftekharm.vectorizesvgfromclipboard.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
